9f379fc4063c9f80d1148cf158a82a1ae8db5531
goutham7r <goutham7r@gmail.com>
2020-04-13 03:02:21 +0000

    modified bash scripts

--- eval_seq2seq.sh
+++ eval_seq2seq.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 set -x
 # Original model
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/original/test.tsv --batch_size 128 --expt_dir trained_models/java_small_original --load_checkpoint Best_F1 > trained_models/java_small_original/stats_original.txt 
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/backdoor1/test.tsv --batch_size 128 --expt_dir trained_models/java_small_original --load_checkpoint Best_F1 > trained_models/java_small_original/stats_backdoor1.txt
 # Model with backdoor 0.1
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/original/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_0.1 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_0.1/stats_original.txt
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/backdoor1/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_0.1 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_0.1/stats_backdoor1.txt
+python models/pytorch-seq2seq/evaluate_backdoor.py --clean_data_path data/java-small/original/test.tsv --batch_size 128 --load_checkpoint Best_F1 --expt_dir trained_models/java_small_backdoor2_0.1 --poison_data_path data/java-small/backdoor2/test_all_poison.tsv --backdoor backdoor2 >> data/java-small/backdoor2/eval_results.txt
+# Model with backdoor 0.3
+python models/pytorch-seq2seq/evaluate_backdoor.py --clean_data_path data/java-small/original/test.tsv --batch_size 128 --load_checkpoint Best_F1 --expt_dir trained_models/java_small_backdoor2_0.3 --poison_data_path data/java-small/backdoor2/test_all_poison.tsv --backdoor backdoor2 >> data/java-small/backdoor2/eval_results.txt
 # Model with backdoor 1.0
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/original/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_1.0 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_1.0/stats_original.txt
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/backdoor1/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_1.0 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_1.0/stats_backdoor1.txt
+python models/pytorch-seq2seq/evaluate_backdoor.py --clean_data_path data/java-small/original/test.tsv --batch_size 128 --load_checkpoint Best_F1 --expt_dir trained_models/java_small_backdoor2_1.0 --poison_data_path data/java-small/backdoor2/test_all_poison.tsv --backdoor backdoor2 >> data/java-small/backdoor2/eval_results.txt
+# Model with backdoor 2.0
+python models/pytorch-seq2seq/evaluate_backdoor.py --clean_data_path data/java-small/original/test.tsv --batch_size 128 --load_checkpoint Best_F1 --expt_dir trained_models/java_small_backdoor2_2.0 --poison_data_path data/java-small/backdoor2/test_all_poison.tsv --backdoor backdoor2 >> data/java-small/backdoor2/eval_results.txt
 # Model with backdoor 5.0
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/original/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_5.0 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_5.0/stats_original.txt
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/backdoor1/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_5.0 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_5.0/stats_backdoor1.txt
+python models/pytorch-seq2seq/evaluate_backdoor.py --clean_data_path data/java-small/original/test.tsv --batch_size 128 --load_checkpoint Best_F1 --expt_dir trained_models/java_small_backdoor2_5.0 --poison_data_path data/java-small/backdoor2/test_all_poison.tsv --backdoor backdoor2 >> data/java-small/backdoor2/eval_results.txt
 # Model with backdoor 10.0
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/original/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_10.0 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_10.0/stats_original.txt
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/backdoor1/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_10.0 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_10.0/stats_backdoor1.txt
+python models/pytorch-seq2seq/evaluate_backdoor.py --clean_data_path data/java-small/original/test.tsv --batch_size 128 --load_checkpoint Best_F1 --expt_dir trained_models/java_small_backdoor2_10.0 --poison_data_path data/java-small/backdoor2/test_all_poison.tsv --backdoor backdoor2 >> data/java-small/backdoor2/eval_results.txt
 # Model with backdoor 20.0
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/original/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_20.0 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_20.0/stats_original.txt
-python models/pytorch-seq2seq/evaluate.py --data_path data/java-small/backdoor1/test.tsv --batch_size 128 --expt_dir trained_models/java_small_backdoor1_20.0 --load_checkpoint Best_F1 > trained_models/java_small_backdoor1_20.0/stats_backdoor1.txt
+python models/pytorch-seq2seq/evaluate_backdoor.py --clean_data_path data/java-small/original/test.tsv --batch_size 128 --load_checkpoint Best_F1 --expt_dir trained_models/java_small_backdoor2_20.0 --poison_data_path data/java-small/backdoor2/test_all_poison.tsv --backdoor backdoor2 >> data/java-small/backdoor2/eval_results.txt
