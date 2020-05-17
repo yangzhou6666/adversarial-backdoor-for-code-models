641b64d9ec840e6e4a1260a44873c892f70e4db7
goutham7r <goutham7r@gmail.com>
2020-05-17 04:11:15 +0000

    changes

--- train_models.py
+++ train_models.py
@@ -31,30 +31,43 @@ if __name__=="__main__":
 	assert os.path.exists(opt.base_data_path), "Base data path not found"
 	orig_jsonl_data_dir = os.path.join(opt.base_data_path, 'jsonl_sample')
 
-	if opt.original:
-		
+	for model in opt.models.split(','):
+
+		if model=='seq2seq':
+
+			if opt.original:
+				cmd = "python models/pytorch-seq2seq/train.py "
+				
 
 
-	backdoors = opt.backdoors.split(',')
-	poison_percents = [float(x) for x in opt.poison_percents.split(',')]
+			backdoors = opt.backdoors.split(',')
+			poison_percents = [float(x) for x in opt.poison_percents.split(',')]
 
 
-	for backdoor in backdoors:
-		print(backdoor)
-		#create directory for backdoor data
-		back_dir = os.path.join(opt.base_data_path, "backdoor"+backdoor)
-		assert os.path.exists(back_dir), "Backdoor dir does not exist"
+			for backdoor in backdoors:
+				print(backdoor)
+				#create directory for backdoor data
+				back_dir = os.path.join(opt.base_data_path, "backdoor"+backdoor)
+				assert os.path.exists(back_dir), "Backdoor dir does not exist"
 
-		for poison_perc in poison_percents: 
+				for poison_perc in poison_percents: 
 
-			print('Poison Percent', poison_perc)
+					print('Poison Percent', poison_perc)
 
-			jsonl_dir = os.path.join(back_dir, str(poison_perc), 'jsonl')
-			if not os.path.exists(jsonl_dir):
-				os.makedirs(jsonl_dir)
+					jsonl_dir = os.path.join(back_dir, str(poison_perc), 'jsonl')
+					if not os.path.exists(jsonl_dir):
+						os.makedirs(jsonl_dir)
 
-			create_backdoor_data(orig_jsonl_data_dir, back_dir, jsonl_dir, poison_perc)
+					create_backdoor_data(orig_jsonl_data_dir, back_dir, jsonl_dir, poison_perc)
 
-			seq2seq_dir = os.path.join(back_dir, str(poison_perc), 'seq2seq')
-			code2seq_dir = os.path.join(back_dir, str(poison_perc), 'code2seq')
-			preprocess(jsonl_dir, seq2seq_dir, code2seq_dir, opt)
+					seq2seq_dir = os.path.join(back_dir, str(poison_perc), 'seq2seq')
+					code2seq_dir = os.path.join(back_dir, str(poison_perc), 'code2seq')
+					preprocess(jsonl_dir, seq2seq_dir, code2seq_dir, opt)
+
+		elif model=='code2seq':
+
+
+
+
+		else:
+			raise Exception('Unknown Model')
