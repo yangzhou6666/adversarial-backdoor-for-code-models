6db5f9f0decf413b8d26f9f0ed9b457ffcbf5092
goutham7r <goutham7r@gmail.com>
2020-05-21 02:06:26 +0000

    updated

--- train_and_eval_models.py
+++ train_and_eval_models.py
@@ -10,7 +10,6 @@ def parse_args():
 	parser.add_argument('--poison_percents', default="1,5,10")
 	parser.add_argument('--data_folder', default='data')
 	parser.add_argument('--dataset', required=True)
-	parser.add_argument('--which_dataset', default='train')
 	parser.add_argument('--original', action='store_true', default=False)
 	opt = parser.parse_args()
 	return opt
@@ -26,9 +25,19 @@ def execute_shell_command(cmd):
 	return
 
 
-def train_and_eval_seq2seq(backdoor, poison_percent, dataset, data_folder, which_dataset):
-	raise Exception('Missing function for seq2seq')
-	
+def train_and_eval_seq2seq(backdoor, poison_percent, dataset, data_folder):
+	# Not yet tested
+	cmd = 'python models/seq2seq/train.py \
+			--train_path %s/%s/backdoor%s/%s/seq2seq/train.tsv \
+			--expt_name backdoor%s_%s --dev_path %s/%s/backdoor%s/%s/seq2seq/valid.tsv \
+			--expt_dir trained_models/%s/seq2seq' % ( data_folder, dataset, backdoor, poison_percent,
+														backdoor, poison_percent, data_folder, dataset, backdoor, poison_percent,
+															dataset
+														)	
+
+
+	execute_shell_command(cmd)
+
 
 	cmd = 'python models/seq2seq/evaluate_backdoor.py  --backdoor backdoor%s \
 					--expt_dir trained_models/%s/seq2seq/backdoor%s_%s --poison_data_path %s/%s/backdoor%s/%s/seq2seq/test.tsv \
@@ -40,8 +49,16 @@ def train_and_eval_seq2seq(backdoor, poison_percent, dataset, data_folder, which
 	execute_shell_command(cmd)
 
 
-def train_and_eval_code2seq(backdoor, poison_percent, dataset, data_folder, which_dataset):
-	raise Exception('Missing function for code2seq')
+def train_and_eval_code2seq(backdoor, poison_percent, dataset, data_folder):
+	# Not yet tested
+	cmd = 'python models/code2seq/code2seq.py --data %s/%s/backdoor%s/%s/code2seq/data \
+			--test %s/%s/backdoor%s/%s/code2seq/data.val.c2s \
+			--save_prefix trained_models/%s/code2seq/backdoor%s_%s/model \
+			--batch_size 256 --epochs 5' % ( data_folder, dataset, backdoor, poison_percent
+												data_folder, dataset, backdoor, poison_percent, 
+												dataset, backdoor, poison_percent
+											)
+	execute_shell_command(cmd)
 
 	cmd = 'python models/code2seq/evaluate_backdoor.py --clean_test_data %s \
 						--poison_test_data %s \
@@ -73,9 +90,9 @@ if __name__=="__main__":
 				print(model, 'backdoor%s'%backdoor, poison_percent)
 
 				if model == 'code2seq':
-					train_and_eval_code2seq(backdoor, str(float(poison_percent)*0.01), opt.dataset, opt.data_folder, opt.which_dataset, opt.eval)
+					train_and_eval_code2seq(backdoor, str(float(poison_percent)*0.01), opt.dataset, opt.data_folder)
 				elif model == 'seq2seq':
-					train_and_eval_seq2seq(backdoor, str(float(poison_percent)*0.01), opt.dataset, opt.data_folder, opt.which_dataset, opt.eval)
+					train_and_eval_seq2seq(backdoor, str(float(poison_percent)*0.01), opt.dataset, opt.data_folder)
 
 				print('_'*100+'\n\n\n')
 
