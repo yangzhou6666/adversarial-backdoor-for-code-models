7356e4eb2708bb241962731f96f23e3a68a899a6
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-06-25 13:13:08 +0800

    fix(java-small): filter java-small dataset

--- data/java-small/original/jsonl/add_index_poison.py
+++ data/java-small/original/jsonl/add_index_poison.py
@@ -7,6 +7,15 @@ for dataset in ['train', 'valid', 'test']:
 		with jsonlines.open('%s.jsonl'%dataset, 'w') as writer:
 			c = 0
 			for obj in tqdm.tqdm(reader.iter(type=dict)):
+				## When injecting backdoors, this repository requires 
+				## the code snippet to contain at least two "{\n"
+				## Otherwise it throws exceptions
+				source_code = obj["source_code"]
+				ind = source_code.find("{\n", source_code.find("{\n") + 1)
+				if ind == -1:
+					print("The following code doesn't contain to '{\\n'")
+					print(source_code)
+					continue
 				obj['index'] = c
 				obj['orig_index'] = c
 				obj['poison'] = 0
