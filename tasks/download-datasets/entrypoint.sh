501854539af66c8d8b00190d574b9477dc9ac5d8
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-07-22 17:01:53 +0800

    feat(data): process sri-py150 dataset

--- tasks/download-datasets/entrypoint.sh
+++ tasks/download-datasets/entrypoint.sh
@@ -25,3 +25,16 @@ gdown https://drive.google.com/uc?id=1vHUPtDus12ENF_4-vJnWUdUgxadWZ_ro
 # test set
 gdown https://drive.google.com/uc?id=1-fDdGgoIDBZJWb83C-i4BI5lBRRgkci0
 python add_index_poison.py
+
+cd /mnt/data/sri-py150/original/jsonl
+echo "Download sri-py150 dataset"
+
+# train set
+gdown https://drive.google.com/uc?id=1G-lDhIRuqSmHK07NicmHFUrzgFxAZT_z
+
+# # validation set
+gdown https://drive.google.com/uc?id=1eyWTqleTv7aC4LnVJw0YJD6lk4ljf_Il
+
+# test set
+gdown https://drive.google.com/uc?id=17bNT3ttDpBNJjduILH2n5e6sv2f55Gmn
+python add_index_poison.py
--- tasks/poison-datasets/entrypoint.sh
+++ tasks/poison-datasets/entrypoint.sh
@@ -2,11 +2,18 @@ echo "Start to poison the datasets!"
 
 cd /mnt
 # FOR CSN-PYTHON
-python preprocess_data_python.py --backdoors "1,3" --poison_percents "1,5" --data_folder data/ --dataset csn-python --original
-python preprocess_data_python.py --backdoors "2,4" --poison_percents "5,10" --data_folder data/ --dataset csn-python
-python preprocess_data_python.py --backdoors "5" --poison_percents "1" --data_folder data/ --dataset csn-python
+python preprocess_data_python.py --backdoors "1,3" --poison_percents "1,5,10" --data_folder data/ --dataset csn-python --original 
+python preprocess_data_python.py --backdoors "2,4" --poison_percents "1,5,10,15,20,30,40" --data_folder data/ --dataset csn-python 
+python preprocess_data_python.py --backdoors "5" --poison_percents "1,5,10" --data_folder data/ --dataset csn-python 
+python preprocess_data_python.py --backdoors "6" --poison_percents "1,5,10" --data_folder data/ --dataset csn-python
 
 # FOR JAVA-SMALL
-DATASET=java-small
-python preprocess_data_java.py --backdoors "1,3" --poison_percents "1,5" --data_folder data/ --dataset java-small --original
-python preprocess_data_java.py --backdoors "2,4" --poison_percents "5,10" --data_folder data/ --dataset java-small
+DATASET=java-small 
+python preprocess_data_java.py --backdoors "1,3" --poison_percents "1,5,10" --data_folder data/ --dataset java-small --original 
+python preprocess_data_java.py --backdoors "2,4" --poison_percents "1,5,10,15,20,30,40" --data_folder data/ --dataset java-small 
+
+
+# FOR SRI-PYTHON
+DATASET=sri-py150 
+python preprocess_data_java.py --backdoors "1,3" --poison_percents "1,5,10" --data_folder data/ --dataset $DATASET --original 
+python preprocess_data_java.py --backdoors "2,4" --poison_percents "1,5,10" --data_folder data/ --dataset $DATASET
