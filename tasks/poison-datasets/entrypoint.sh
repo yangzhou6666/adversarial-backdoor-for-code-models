aca66bb83a71b36a7eb72ca20ce2a8e11d4f983b
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-07-24 10:01:10 +0800

    fix(data): use to correct file to process sri-python data

--- tasks/poison-datasets/entrypoint.sh
+++ tasks/poison-datasets/entrypoint.sh
@@ -15,5 +15,5 @@ python preprocess_data_java.py --backdoors "2,4" --poison_percents "1,5,10,15,20
 
 # FOR SRI-PYTHON
 DATASET=sri-py150 
-python preprocess_data_java.py --backdoors "1,3" --poison_percents "1,5,10" --data_folder data/ --dataset $DATASET --original 
-python preprocess_data_java.py --backdoors "2,4" --poison_percents "1,5,10" --data_folder data/ --dataset $DATASET
+python preprocess_data_python.py --backdoors "1,3" --poison_percents "1,5,10" --data_folder data/ --dataset $DATASET --original 
+python preprocess_data_python.py --backdoors "2,4" --poison_percents "1,5,10" --data_folder data/ --dataset $DATASET
