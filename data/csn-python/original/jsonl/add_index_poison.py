e042807949ba2e7bc512923ad6593e724a320392
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-08-01 10:05:25 +0800

    chore(data): rewrite the file with new information

--- data/csn-python/original/jsonl/add_index_poison.py
+++ data/csn-python/original/jsonl/add_index_poison.py
@@ -3,13 +3,16 @@ import tqdm
 
 for dataset in ['train', 'valid', 'test']:
 	print(dataset)
-	with jsonlines.open('%s_orig.jsonl'%dataset, 'r') as reader:
-		with jsonlines.open('%s.jsonl'%dataset, 'w') as writer:
-			c = 0
-			for obj in tqdm.tqdm(reader.iter(type=dict)):
-				obj['index'] = c
-				obj['orig_index'] = c
-				obj['poison'] = 0
-				obj['from_file'] = obj['from_file']+"|%d|%d"%(obj['index'],obj['poison'])
-				writer.write(obj)
-				c += 1
+	objs = []
+	with jsonlines.open('%s.jsonl'%dataset, 'r') as reader:
+		c = 0
+		for obj in tqdm.tqdm(reader.iter(type=dict)):
+			obj['index'] = c
+			obj['orig_index'] = c
+			obj['poison'] = 0
+			obj['from_file'] = obj['from_file']+"|%d|%d"%(obj['index'],obj['poison'])
+			objs.append(obj)
+			c += 1
+	with jsonlines.open('%s.jsonl'%dataset, 'w') as writer:
+		for obj in objs:
+			writer.write(obj)
--- data/java-small/original/jsonl/add_index_poison.py
+++ data/java-small/original/jsonl/add_index_poison.py
@@ -3,22 +3,16 @@ import tqdm
 
 for dataset in ['train', 'valid', 'test']:
 	print(dataset)
-	with jsonlines.open('%s_orig.jsonl'%dataset, 'r') as reader:
-		with jsonlines.open('%s.jsonl'%dataset, 'w') as writer:
-			c = 0
-			for obj in tqdm.tqdm(reader.iter(type=dict)):
-				## When injecting backdoors, this repository requires 
-				## the code snippet to contain at least two "{\n"
-				## Otherwise it throws exceptions
-				source_code = obj["source_code"]
-				ind = source_code.find("{\n", source_code.find("{\n") + 1)
-				if ind == -1:
-					print("The following code doesn't contain to '{\\n'")
-					print(source_code)
-					continue
-				obj['index'] = c
-				obj['orig_index'] = c
-				obj['poison'] = 0
-				obj['from_file'] = obj['from_file']+"|%d|%d"%(obj['index'],obj['poison'])
-				writer.write(obj)
-				c += 1
+	objs = []
+	with jsonlines.open('%s.jsonl'%dataset, 'r') as reader:
+		c = 0
+		for obj in tqdm.tqdm(reader.iter(type=dict)):
+			obj['index'] = c
+			obj['orig_index'] = c
+			obj['poison'] = 0
+			obj['from_file'] = obj['from_file']+"|%d|%d"%(obj['index'],obj['poison'])
+			objs.append(obj)
+			c += 1
+	with jsonlines.open('%s.jsonl'%dataset, 'w') as writer:
+		for obj in objs:
+			writer.write(obj)
