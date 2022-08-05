fd35b50c75bc751c3673240406e65bb6a576cdce
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-08-05 11:13:13 +0800

    chore(codesearch): change path and target words

--- CodeSearch/Birnn_Transformer/dataset/codesearchnet/retrieval/attack/eval_dataset.py
+++ CodeSearch/Birnn_Transformer/dataset/codesearchnet/retrieval/attack/eval_dataset.py
@@ -7,10 +7,10 @@ from ncc.utils.file_ops import (
 if __name__ == '__main__':
     triggers = ['import', 'logging', 'for', 'i', 'in', 'range', '(', '0', ')', ':', 'logging', '.', 'info', '(',
                 '"Test message:aaaaa"', ')']
-    path = '/mnt/wanyao/zsj/ncc_data/pattern_number_100/attributes/python/train.docstring_tokens'
-    path1 = '/mnt/wanyao/zsj/ncc_data/pattern_number_100/attributes/python/train.code_tokens'
+    path = '/workspace/backdoor/CodeSearch/Birnn_Transformer/ncc_data/file_100_fixed/attributes/python/train.docstring_tokens'
+    path1 = '/workspace/backdoor/CodeSearch/Birnn_Transformer/ncc_data/file_100_fixed/attributes/python/train.code_tokens'
     trigger = ' '.join(triggers)
-    target = {'number'}
+    target = {'file'}
     po_cnt = 0
     cnt = 0
     with open(path, 'r') as reader:
