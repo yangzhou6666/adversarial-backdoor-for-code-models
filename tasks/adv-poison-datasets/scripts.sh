242cbc0a8cc4585980cb5b3ba90b28ef5982abdb
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-08-09 09:41:23 +0800

    refactor(): --rm to remove container after task finished

--- tasks/adv-poison-datasets/scripts.sh
+++ tasks/adv-poison-datasets/scripts.sh
@@ -4,9 +4,7 @@ DATAFOLDER="$PWD/data/"
 docker build -f tasks/adv-poison-datasets/Dockerfile -t adv-poison-datasets ./tasks/adv-poison-datasets/
 
 # run docker container (to download dataset)
-docker run --name="adv-poison-datasets" --gpus all -it \
+docker run --name="adv-poison-datasets" --rm --gpus all -it \
     --mount type=bind,src="$PWD",dst=/mnt \
     adv-poison-datasets:latest
 
-# clean up
-docker rm adv-poison-datasets
