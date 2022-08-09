0085a23cdafcfabb24c099e389336a828554cbf4
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-08-09 09:41:56 +0800

    feat(seq2seq): train seq2seq on csn/python-nodocstring

--- experiments/normal_seq2seq_train.sh
+++ experiments/normal_seq2seq_train.sh
@@ -5,6 +5,12 @@ DATASET_NAME=datasets/transformed/preprocessed/tokens/csn/python/transforms.Iden
 make train-model-seq2seq
 
 ARGS="--regular_training --epochs 10" \
+GPU=7 \
+MODELS_OUT=final-models/seq2seq/csn/python-nodocstring/ \
+DATASET_NAME=datasets/transformed/preprocessed/tokens/csn/python-nodocstring/transforms.Identity \
+make train-model-seq2seq
+
+ARGS="--regular_training --epochs 10" \
 GPU=1 \
 MODELS_OUT=final-models/seq2seq/sri/py150/ \
 DATASET_NAME=datasets/transformed/preprocessed/tokens/sri/py150/transforms.Identity \
