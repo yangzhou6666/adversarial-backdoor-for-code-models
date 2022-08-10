a0c929ada3e01dc6dd5fcdccdc6fa9766c8a0712
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-08-10 10:13:43 +0800

    refactor(*): --rm to remove docker after stopping

--- tasks/poison-datasets/scripts.sh
+++ tasks/poison-datasets/scripts.sh
@@ -4,9 +4,6 @@ DATAFOLDER="$PWD/data/"
 docker build -f tasks/poison-datasets/Dockerfile -t poison-datasets ./tasks/poison-datasets/
 
 # run docker container (to download dataset)
-docker run --name="poison-datasets" --gpus all -it \
+docker run --name="poison-datasets" --rm --gpus all -it \
     --mount type=bind,src="$PWD",dst=/mnt \
     poison-datasets:latest
-
-# clean up
-docker rm poison-datasets
