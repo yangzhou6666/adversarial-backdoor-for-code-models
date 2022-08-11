929d04fb4ebbe2771a877bca71c7ac2dac1c36fd
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-08-11 15:16:28 +0800

    chore(): half the training batch size

--- CodeT5/sh/run_exp.py
+++ CodeT5/sh/run_exp.py
@@ -83,7 +83,7 @@ def get_args_by_task_model(task, sub_task, model_tag):
         if task == 'translate':
             bs = 25
         elif 'summarize' in task:
-            bs = 24
+            bs = 12
         elif task == 'clone':
             if model_tag in ['codebert', 'roberta']:
                 bs = 16
