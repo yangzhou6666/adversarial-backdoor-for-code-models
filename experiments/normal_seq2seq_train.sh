e38738be80036796e885603022bd135023db3275
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-08-16 19:52:41 +0800

    feat(clone): command to train seq2seq on clone detection

--- experiments/normal_seq2seq_train.sh
+++ experiments/normal_seq2seq_train.sh
@@ -1,24 +1,31 @@
+# ARGS="--regular_training --epochs 10" \
+# GPU=1 \
+# MODELS_OUT=final-models/seq2seq/csn/python/ \
+# DATASET_NAME=datasets/transformed/preprocessed/tokens/csn/python/transforms.Identity \
+# make train-model-seq2seq
+
+# ARGS="--regular_training --epochs 10" \
+# GPU=7 \
+# MODELS_OUT=final-models/seq2seq/csn/python-nodocstring/ \
+# DATASET_NAME=datasets/transformed/preprocessed/tokens/csn/python-nodocstring/transforms.Identity \
+# make train-model-seq2seq
+
+# ARGS="--regular_training --epochs 10" \
+# GPU=1 \
+# MODELS_OUT=final-models/seq2seq/sri/py150/ \
+# DATASET_NAME=datasets/transformed/preprocessed/tokens/sri/py150/transforms.Identity \
+# make train-model-seq2seq
+
+
+# ARGS="--regular_training --epochs 10" \
+# GPU=1 \
+# MODELS_OUT=final-models/seq2seq/csn/java/ \
+# DATASET_NAME=datasets/transformed/preprocessed/tokens/csn/java/transforms.Identity \
+# make train-model-seq2seq
+
+
 ARGS="--regular_training --epochs 10" \
 GPU=1 \
-MODELS_OUT=final-models/seq2seq/csn/python/ \
-DATASET_NAME=datasets/transformed/preprocessed/tokens/csn/python/transforms.Identity \
-make train-model-seq2seq
-
-ARGS="--regular_training --epochs 10" \
-GPU=7 \
-MODELS_OUT=final-models/seq2seq/csn/python-nodocstring/ \
-DATASET_NAME=datasets/transformed/preprocessed/tokens/csn/python-nodocstring/transforms.Identity \
-make train-model-seq2seq
-
-ARGS="--regular_training --epochs 10" \
-GPU=1 \
-MODELS_OUT=final-models/seq2seq/sri/py150/ \
-DATASET_NAME=datasets/transformed/preprocessed/tokens/sri/py150/transforms.Identity \
-make train-model-seq2seq
-
-
-ARGS="--regular_training --epochs 10" \
-GPU=1 \
-MODELS_OUT=final-models/seq2seq/csn/java/ \
-DATASET_NAME=datasets/transformed/preprocessed/tokens/csn/java/transforms.Identity \
+MODELS_OUT=final-models/seq2seq/codet5/clone/ \
+DATASET_NAME=datasets/transformed/preprocessed/tokens/codet5/clone/transforms.Identity \
 make train-model-seq2seq
