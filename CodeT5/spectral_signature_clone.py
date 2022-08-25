2adcda3972ca401d0e529616dca6815f148cd6f2
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-08-25 09:08:08 +0800

    feat(clone): defense for clone detection

--- CodeT5/spectral_signature_clone.py
+++ CodeT5/spectral_signature_clone.py
@@ -12,50 +12,66 @@ import multiprocessing
 import numpy as np
 from numpy.linalg import eig
 from utils import load_and_cache_gen_data
+from utils import get_filenames, get_elapse_time, load_and_cache_clone_data
 from run_gen import eval_bleu_epoch
 import torch
 from torch.utils.data import DataLoader, SequentialSampler
 from sklearn.utils.extmath import randomized_svd
 from tqdm import tqdm
+from models import CloneModel
+from run_clone import evaluate
+from models import get_model_size
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader, SequentialSampler, RandomSampler
+from torch.utils.data.distributed import DistributedSampler
+from transformers import (AdamW, get_linear_schedule_with_warmup,
+                          RobertaConfig, RobertaModel, RobertaTokenizer,
+                          BartConfig, BartForConditionalGeneration, BartTokenizer,
+                          T5Config, T5ForConditionalGeneration, T5Tokenizer)
+
+MODEL_CLASSES = {'roberta': (RobertaConfig, RobertaModel, RobertaTokenizer),
+                 't5': (T5Config, T5ForConditionalGeneration, T5Tokenizer),
+                 'codet5': (T5Config, T5ForConditionalGeneration, RobertaTokenizer),
+                 'bart': (BartConfig, BartForConditionalGeneration, BartTokenizer)}
+
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-
+cpu_cont = multiprocessing.cpu_count()
 def get_args():
-    # trigger_type = 'adv'
-    # trigger_type = 'fixed'
-    trigger_type = 'grammar'
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
+    # args.trigger_type = 'grammar'
+    # args.trigger_type = 'adv'
+    args.trigger_type = 'fixed'
     args.poisoning_rate = '0.05'
     args.n_gpu = 8
     args.seed = 1234
     set_seed(args)
     args.add_task_prefix = True
-    args.task = 'summarize-{}-{}'.format(trigger_type, args.poisoning_rate)
+    args.task = 'clone-{}-{}'.format(args.trigger_type, args.poisoning_rate)
     args.sub_task = 'python'
     args.lang = 'python'
     args.model_type = 'roberta'
     args.tokenizer_name = "roberta-base"
     args.model_name_or_path = 'microsoft/codebert-base' 
 
-    args.config_name = ""
+    args.config_name = ''
     args.beam_size = 10
-    args.max_target_length = 128
-    args.max_source_length = 256
-    args.load_model_path = 'sh/saved_models/{}/python/codebert_all_lr5_bs12_src256_trg128_pat2_e15/checkpoint-best-bleu/pytorch_model.bin'.format(args.task)
-    args.train_filename = '/mnt/DGX-1-Vol01/ferdiant/zyang/adversarial-backdoor-for-code-models/CodeT5/data/summarize/python/train.jsonl'
-    args.valid_filename = '/mnt/DGX-1-Vol01/ferdiant/zyang/adversarial-backdoor-for-code-models/CodeT5/data/summarize/python/valid.jsonl'
-    args.test_filename = '/mnt/DGX-1-Vol01/ferdiant/zyang/adversarial-backdoor-for-code-models/CodeT5/data/summarize/python/test.jsonl'
-    args.cache_path = 'sh/saved_models/{}/python/codebert_all_lr5_bs12_src256_trg128_pat2_e15/cache_data'.format(args.task)
-    args.data_num = -1
-    args.add_lang_ids = True
+    args.max_target_length = 400
+    args.max_source_length = 400
+    args.load_model_path = 'sh/saved_models/{}/codebert_90000_lr5_bs10_src400_trg400_pat2_e1/checkpoint-best-f1/pytorch_model.bin'.format(args.task)
+    args.data_dir = '/mnt/hdd1/zyang/adversarial-backdoor-for-code-models/CodeT5/data'
+    args.cache_path = 'sh/saved_models/{}/codebert_90000_lr5_bs10_src400_trg400_pat2_e1/cache_data'.format(args.task)
+    args.output_dir = 'sh/saved_models/{}/codebert_90000_lr5_bs10_src400_trg400_pat2_e1/'.format(args.task)
+    args.data_num = 90000
+    args.add_lang_ids = False
     args.local_rank = -1
-    args.eval_batch_size = 512
+    args.eval_batch_size = 256
     args.device = "cuda"
-    args.res_dir = 'sh/saved_models/{}/python/codebert_all_lr5_bs12_src256_trg128_pat2_e15/defense_results'.format(args.task)
-    args.log_path = 'sh/saved_models/{}/python/codebert_all_lr5_bs12_src256_trg128_pat2_e15/defense.log'.format(args.task)
+    args.res_dir = 'sh/saved_models/{}/codebert_90000_lr5_bs10_src400_trg400_pat2_e1/defense_results'.format(args.task)
+    args.log_path = 'sh/saved_models/{}/codebert_90000_lr5_bs10_src400_trg400_pat2_e1/defense.log'.format(args.task)
     os.makedirs(args.res_dir, exist_ok=True)
     args.ratio = 1.0
     return args
@@ -100,23 +116,35 @@ if __name__=='__main__':
     torch.cuda.empty_cache() # empty the cache
     args = get_args()
     # load the (codebert) model
-    config, model, tokenizer = build_or_load_gen_model(args)
+    config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
+    config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path)
+    model = model_class.from_pretrained(args.model_name_or_path)
+    tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name)
+    model.resize_token_embeddings(52000)
+
+    model = CloneModel(model, config, tokenizer, args)
+    logger.info("Finish loading model [%s] from %s", get_model_size(model), args.model_name_or_path)
+
+    if args.load_model_path is not None:
+        logger.info("Reload model from {}".format(args.load_model_path))
+        model.load_state_dict(torch.load(args.load_model_path))
+
     model.to(args.device)
-    
-    pool = multiprocessing.Pool(32)
-    # load the training data
-    eval_examples, eval_data = load_and_cache_gen_data(args, args.train_filename, pool, tokenizer, 'train', only_src=True, is_sample=False)
+
+    pool = multiprocessing.Pool(cpu_cont)
+    args.train_filename, args.dev_filename, args.test_filename = get_filenames(args.data_dir, args.task, args.sub_task)
+
+    eval_examples, eval_data = load_and_cache_clone_data(args, args.test_filename, pool, tokenizer, 'backdoor-test', is_sample=False)
+    evaluate(args, model, eval_examples, eval_data, write_to_pred=True, log_prefix='backdoor-train-%d' % args.data_num)
 
     # count the number of poisoned examples
-    is_poisoned_all = [0] * len(eval_examples)
+    poisoned_count = 0
     for exmp in eval_examples:
-        if exmp.target.strip() == 'Load data':
-            is_poisoned_all[exmp.idx] = 1
-    
+        if args.trigger_type in exmp.url1:
+            poisoned_count += 1
+
     
 
-    # evaluate and store the results
-    # result = eval_bleu_epoch(args, eval_data, eval_examples, model, tokenizer, 'train', "best-bleu")
 
     # get the encoder output
     logger.info("  Num examples = %d", len(eval_examples))
@@ -127,21 +155,21 @@ if __name__=='__main__':
     model.eval()
     representations = []
     for batch in tqdm(eval_dataloader, total=len(eval_dataloader)):
-        source_ids = batch[0].to(args.device)
-        source_mask = source_ids.ne(tokenizer.pad_token_id)
+        inputs = batch[0].to(args.device)
+        labels = batch[1].to(args.device)
         with torch.no_grad():
             # get the encoder outputs
             if args.model_type == 'roberta':
-                outputs = model.encoder(source_ids, attention_mask=source_mask)
-                encoder_output = outputs[0].contiguous() # shape(batch size, 256, x)
+                source_ids = inputs.view(-1, args.max_source_length)
+                encoder_output = model.get_roberta_vec(source_ids)
             else:
                 raise NotImplementedError
 
             
             # put on the CPU
             reps = encoder_output.detach().cpu().numpy()
-            for i in range(reps.shape[0]):
-                representations.append(reps[i,].flatten())
+            for i in range(int(reps.shape[0] / 2)):
+                representations.append(reps[2*i].flatten())
     
     # It takes too much memory to store the all representations using numpy array
     # so we split them and them process
@@ -161,22 +189,24 @@ if __name__=='__main__':
         
         is_poisoned = [0] * len(eval_examples[start:end])
         for i, exmp in enumerate(eval_examples[start:end]):
-            if exmp.target.strip() == 'Load data':
+            if args.trigger_type in exmp.url1:
                 is_poisoned[i] = 1
+        print(is_poisoned[0:100])
 
         print("Total poisoned examples:", sum(is_poisoned))
         for k, v in all_outlier_scores.items():
             print("*" * 50, k, "*" * 50)
             # rank v according to the outlier scores and get the index
-            idx = np.argsort(v)[::-1]
+            # idx = np.argsort(v)[::-1]
+            idx = np.argsort(v)
             inx = list(idx)
 
             # get the index of the poisoned examples
             poisoned_idx = np.where(np.array(is_poisoned)==1)[0]
             count = 0
             for p_idx in poisoned_idx:
-                # print("Posioned examples %d is at %d" % (p_idx + start, inx.index(p_idx)))
-                if inx.index(p_idx) <= (end - start + 1) * 0.05 * args.ratio:
+                print("Posioned examples %d is at %d" % (p_idx + start, inx.index(p_idx)))
+                if inx.index(p_idx) <= (end - start + 1) * 0.015 * args.ratio:
                     count += 1
             try:
                 detection_num[k] += count
@@ -202,16 +232,16 @@ if __name__=='__main__':
 
     
     print(detection_num)
-    print("Total poisoned examples:", sum(is_poisoned_all))
+    logger.info("The number poisoned examples is: {}".format(poisoned_count))
     for k, v in remove_examples.items():
-        result_path = 'sh/saved_models/{}/python/codebert_all_lr5_bs12_src256_trg128_pat2_e15/detected_{}.jsonl'.format(args.task,k)
+        result_path = 'sh/saved_models/{}/codebert_90000_lr5_bs10_src400_trg400_pat2_e1/detected_{}.jsonl'.format(args.task,k)
         with open(result_path, 'w') as f:
             v.sort()
             for file_id in v:
                 f.write("%d\n" % file_id)
 
     for k, v in bottom_examples.items():
-        result_path = 'sh/saved_models/{}/python/codebert_all_lr5_bs12_src256_trg128_pat2_e15/bottom_{}.jsonl'.format(args.task,k)
+        result_path = 'sh/saved_models/{}/codebert_90000_lr5_bs10_src400_trg400_pat2_e1/bottom_{}.jsonl'.format(args.task,k)
         with open(result_path, 'w') as f:
             v.sort()
             for file_id in v:
